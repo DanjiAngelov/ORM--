f6f79fc90a311594f4f995ccc2fe191f074f7b3a
DanjiAngelov <danielangelov728@gmail.com>
2025-04-08 09:19:23 +0300

    Create Airport.cs

---
+++ Airport.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirCompanySystem.Models;
+using project78.BaseModels;
+
+namespace project78
+{
+    public class Airport:BaseModel
+    {
+        [MaxLength(100)]
+        public string AirportName { get; set; }
+        public int? CityId { get; set; }
+        public City? City { get; set; } = null!;
+        public ICollection<Flight> Flights { get; set; } = new List<Flight>();
+    }
+}
